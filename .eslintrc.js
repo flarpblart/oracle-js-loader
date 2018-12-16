d3c31c6250ef9fa8b4852dd72cf422893004c624
hdsturgill <hdsturgill@wcu.edu>
2018-12-16 08:49:55 -0500

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
